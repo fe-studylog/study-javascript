0f1428d3c0122136e51c48e2541aae29f4c2be92
soma0078 <104878713+soma0078@users.noreply.github.com>
2023-12-17 23:26:26 +0900

    add js tutoril

---
+++ JS_Tutorial/01_JS intro.js
@@ -0,0 +1,19 @@
+function hideFunction(){
+    document.getElementById('description').style.display ='none';
+}
+
+function showScript1(){
+    document.getElementById('script1').style.display ='block';
+}
+function showScript2(){
+    document.getElementById('script2').style.display ='block';
+}
+function showScript3(){
+    document.getElementById('script3').style.display ='block';
+}
+function showScript4(){
+    document.getElementById('script4').style.display ='block';
+}
+function showScript5(){
+    document.getElementById('script5').style.display ='block';
+}
